b552307109c75b2199a12a2c7228f97861d55153
Catherine Kim <cat.kim@qut.edu.au>
2022-10-05 14:54:11 +1000

    Changed downloading benthic GBR10 from board.

--- GBR10_map.R
+++ GBR10_map.R
@@ -6,20 +6,38 @@
 #install.packages(c("terra", "tmap", "ozmaps"))
 
 library(terra)
+terraOptions()
+terraOptions(tempdir = "U:/Research/Projects/sef/rrap_rubble/Data _all/Personal_WIP/Catherine/rubble/temp",
+             memfrac = 0.9)
 library(tmap)
 library(ozmaps)
+library(pins)
 
 # Read in the data ----
-benthic <- rast('data/GBR10 GBRMP Benthic.tif')
+# read in file from U drive
+gbrmpa_board <- board_folder("U:\\Research\\Projects\\sef\\rrap_rubble\\Data _all\\GBRMPA_board")
+
+benthic <- pin_download(gbrmpa_board, 'GBR10_Benthic') %>% rast()
+#benthic <- rast('../data/GBR10 GBRMP Benthic.tif')
+
+# read in file locally
+# benthic <- rast('data/GBR10 GBRMP Benthic.tif')
 benthic
 crs(benthic)
 
 # basic plot
 plot(benthic)
+qtm(benthic)
 
 # tmap plot ----
 # too big, try aggregating times 10
-agg_benthic <- terra::aggregate(benthic, fact = 20)
+#agg_benthic <- terra::aggregate(benthic, fun = "modal", fact = 100)
+#writeRaster(agg_benthic, 'processed_data/agg100_GBR10.tif')
+
+x <- rast('processed_data/agg100_GBR10.tif')
+plot(x)
+qtm(x) #looks like has nas?
+plot(agg_benthic)
 
 tm_shape(agg_benthic) +
   tm_raster()
